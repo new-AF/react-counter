0782b2cea4c4b9aeaad6e1dd62aeffba79f0d9a3
Abdullah Fatota <53046436+new-AF@users.noreply.github.com>
2023-06-04 23:26:32 +0200

    Update App.jsx App.css
    style .counter buttons
    
    add decrement reset and setMax callbacks

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,31 @@ function App() {
   const increment = (event) => {
     setCount((prevValue) => prevValue + BigInt(1));
   };
+  const decrement = (event) => {
+    setCount((prevValue) => prevValue - BigInt(1));
+  };
+  const reset = (event) => {
+    setCount((prevValue) => BigInt(0));
+  };
+  const setMax = (event) => {
+    setCount((prevValue) => BigInt(Number.MAX_SAFE_INTEGER));
+  };
   return (
     <React.Fragment>
       <h1 id="header">React Counter</h1>
-      <button id="counter" onClick={increment}>
-        Count is {`${count}`}
+      <p className="text">Count is</p>
+      <p id="number" className="text">{`${count}`}</p>
+      <button className="counter" onClick={increment}>
+        Increment
+      </button>
+      <button className="counter" onClick={decrement}>
+        Decrement
+      </button>
+      <button className="counter" onClick={reset}>
+        Set to <code>0</code>
+      </button>
+      <button className="counter" onClick={setMax}>
+        Set to <code>Number.MAX_SAFE_INTEGER</code>
       </button>
       <MadeWith></MadeWith>
       <footer id="footer">by Abdullah Fatota</footer>
