2c38b2b3068df5881583c0d1d21a3f5d29a6c62c
Abdullah Fatota <53046436+new-AF@users.noreply.github.com>
2023-06-04 22:56:43 +0200

    Update MadeWith.jsx MadeWith.css
    add links and styling to stack items

--- src/MadeWith.jsx
+++ src/MadeWith.jsx
@@ -5,15 +5,17 @@ import "./MadeWith.css";
 function MadeWith() {
   // all of  this to avoid repetitive code of multiple <li><img/></li> s
   const stack = [
-    { name: "React library", icon: reactLogo },
-    { name: "Vite tooling", icon: viteLogo },
+    { name: "React library", icon: reactLogo, url: "https://react.dev/" },
+    { name: "Vite tooling", icon: viteLogo, url: "https://vitejs.dev/" },
   ];
 
   // this below specifically
-  const stackItems = stack.map(({ name, icon }) => (
+  const stackItems = stack.map(({ name, icon, url }) => (
     <li className="list-item">
-      <img className="list-item-icon" src={icon} alt={`${name} logo`} />
-      <span>{name}</span>
+      <a className="list-item-link" href={url}>
+        <img className="list-item-link-icon" src={icon} alt={`${name} logo`} />
+        <span className="list-item-link-text">{name}</span>
+      </a>
     </li>
   ));
 
